611219798cc5ad113aa0d57005f85d03d4cd8136
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-06-11 13:07:12 +0300

    added constructor

--- v2/src/main/java/dinobot/game/model/CactusObstacle.java
+++ v2/src/main/java/dinobot/game/model/CactusObstacle.java
@@ -7,6 +7,23 @@ public class CactusObstacle extends Obstacle {
 	public CactusType cactusType;
   
   public CactusObstacle(float x, float y, CactusType cactusType) {
+	  	super(x, y);
+		this.type = ObstacleType.CACTUS;
+		this.cactusType = cactusType;
+		switch (this.cactusType) {
+		case SMALL:
+			this.width = 40;
+			this.height = 60;
+			break;
+		case MEDIUM:
+			this.width = 100;
+			this.height = 80;
+			break;
+		case LARGE:
+			this.width = 140;
+			this.height = 100;
+		default:
+			break;
+		}
   }
-  
 }
