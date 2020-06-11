4f75b0c91b1de2aa3af872eb856c57c59a75aa80
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-06-11 13:01:38 +0300

    skeleton

---
+++ v2/src/main/java/dinobot/game/model/CactusObstacle.java
@@ -0,0 +1,12 @@
+public class CactusObstacle extends Obstacle {
+	
+	public enum CactusType {
+		SMALL, MEDIUM, LARGE
+	};
+	
+	public CactusType cactusType;
+  
+  public CactusObstacle(float x, float y, CactusType cactusType) {
+  }
+  
+}
