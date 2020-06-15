1034c31eef018508187b9a9920e5ef660904781e
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-06-15 12:49:16 +0300

    added class constructor and imports

---
+++ v2/src/main/java/dinobot/game/creation/DinoCreation.java
@@ -0,0 +1,13 @@
+package game.creation;
+import game.model.Dino;
+import neuroevolution.neuralnetwork.NeuralNetwork.FlattenNet;
+
+public class DinoCreation {
+	private static float spawnX;
+	private static float spawnY;
+	
+	
+	private DinoCreation() {
+	}
+  
+}
