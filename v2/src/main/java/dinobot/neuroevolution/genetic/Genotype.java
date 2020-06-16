b6eb0a3b6a9916c91e90d831b2ecb79823a9cd73
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-06-16 11:56:30 +0300

    breed function added

--- v2/src/main/java/dinobot/neuroevolution/genetic/Genotype.java
+++ v2/src/main/java/dinobot/neuroevolution/genetic/Genotype.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 import java.util.List;
 import game.creation.DinoCreation;
 import game.model.Dino;
-import neuroevolution.neuralnetwork.NeuralNetwork;
 import neuroevolution.neuralnetwork.NeuralNetwork.FlattenNet;
 
 public class Genotype {
@@ -26,5 +25,22 @@ public class Genotype {
 	public Genotype(Genotype genome) {
 		this.dino = DinoCreation.getDino(genome.dino.cactusNN.flatten(), genome.dino.birdNN.flatten());
 		this.fitness = 0;
+	}
+	
+	
+	public static List<Genotype> breed(Genotype male, Genotype female, int childCount, float mutationRate, float mutationStdDev) {
+		
+		List<Genotype> children = new ArrayList<Genotype>();
+		for (int ch = 0; ch < childCount; ch++) {
+			FlattenNet childCactusNet = male.dino.cactusNN.flatten();
+			FlattenNet childBirdNet = male.dino.birdNN.flatten();
+			FlattenNet parentCactusNet = female.dino.cactusNN.flatten();
+			FlattenNet parentBirdNet = female.dino.birdNN.flatten();
+			
+			childCactusNet = breedNeuralNetworks(childCactusNet, parentCactusNet, mutationRate, mutationStdDev);
+			childBirdNet = breedNeuralNetworks(childBirdNet, parentBirdNet, mutationRate, mutationStdDev);
+			children.add(new Genotype(childCactusNet, childBirdNet));
+		}
+		return children;
 	}
 }
