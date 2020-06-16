b4fa63030d554ce9dac7e795066625585e93dfd1
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-06-16 10:59:21 +0300

    imports and constructor

---
+++ v2/src/main/java/dinobot/game/creation/BirdCreation.java
@@ -0,0 +1,13 @@
+package game.creation;
+import game.model.BirdObstacle;
+import game.model.Obstacle;
+import game.model.BirdObstacle.BirdHeight;
+
+public class BirdCreation {
+	private static float spawnX;
+	private static float spawnY;
+	private static BirdHeight[] types = BirdHeight.values();
+	
+	private BirdCreation() {	
+	}
+}
