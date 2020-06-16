c46922d1a2712c9f116a1eaa57033df8475d266c
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-06-16 13:16:05 +0300

    evolve

--- v2/src/main/java/dinobot/neuroevolution/genetic/Population.java
+++ v2/src/main/java/dinobot/neuroevolution/genetic/Population.java
@@ -16,6 +16,27 @@ public class Population {
 
 	
 	public void evolve(float elitism, float randomness, float mutationRate, float mutationStdDev, int childCount) {
+		this.normalFitnessDistribution();
+		this.sortByFitness();
+		List<Genotype> nextGeneration = new ArrayList<Genotype>();
+		
+		int eliteCount = Math.round(elitism*this.genomes.size());
+		for (int i = 0; i < eliteCount; i++) {
+			nextGeneration.add(new Genotype(this.genomes.get(i)));
+		}
+		
+		int randomCount = Math.round(randomness*this.genomes.size());
+		for (int i = 0; i < randomCount; i++) {
+			FlattenNet cNet  = this.genomes.get(0).dino.cactusNN.flatten();
+			for (int j = 1; j < cNet.weights.size(); j++) {
+				cNet.weights.set(j, (float) (Math.random()*2 - 1));
+			}
+			FlattenNet bNet = this.genomes.get(0).dino.birdNN.flatten();
+			for (int j = 1; j < bNet.weights.size(); j++) {
+				bNet.weights.set(j, (float) (Math.random()*2 - 1));
+			}
+			nextGeneration.add(new Genotype(cNet, bNet));
+		}
 	}
 	
 	
