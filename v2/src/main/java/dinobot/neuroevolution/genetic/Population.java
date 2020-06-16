f4232e63cbc228ecdfe64109e864f915da82fef0
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-06-16 12:45:03 +0300

    population skeleton

---
+++ v2/src/main/java/dinobot/neuroevolution/genetic/Population.java
@@ -0,0 +1,24 @@
+package neuroevolution.genetic;
+import java.util.ArrayList;
+import java.util.List;
+import neuroevolution.neuralnetwork.NeuralNetwork.FlattenNet;
+
+public class Population {
+	public List <Genotype> genomes;
+	
+	
+	public Population(int populationSize) {
+	}
+
+	
+	public void evolve(float elitism, float randomness, float mutationRate, float mutationStdDev, int childCount) {
+	}
+	
+	
+	private void normalFitnessDistribution() {
+	}
+	
+
+	private void sortByFitness() {
+	}
+}
