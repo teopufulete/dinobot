1a5891f436839feac42e7938e0ffa6c301c9f72c
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-06-19 10:44:34 +0300

    imports

---
+++ v2/src/main/java/dinobot/neuroevolution/genetic/GeneticAlgorithm.java
@@ -0,0 +1,9 @@
+package neuroevolution.genetic;
+import java.util.List;
+import game.creation.DinoCreation;
+import game.model.Dino;
+import game.model.Obstacle;
+
+public class GeneticAlgorithm {
+
+}
