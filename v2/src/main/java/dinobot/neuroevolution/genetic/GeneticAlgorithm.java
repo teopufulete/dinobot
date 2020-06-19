f9a35f7df26d5989eb9eab31efb0ae05741a6876
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-06-19 11:04:55 +0300

    skeleton

--- v2/src/main/java/dinobot/neuroevolution/genetic/GeneticAlgorithm.java
+++ v2/src/main/java/dinobot/neuroevolution/genetic/GeneticAlgorithm.java
@@ -5,5 +5,29 @@ import game.model.Dino;
 import game.model.Obstacle;
 
 public class GeneticAlgorithm {
-
+  public class ObstacleInfo {    
+	}
+	
+	public GeneticAlgorithm() {
+	}
+	
+	public void updatePopulation(List<Obstacle> obstacles) {
+	}
+	
+	public void evolvePopulation() {
+	}
+	
+	public Dino getBestGenome() {
+	}
+	
+	public int getBestScore() {
+	}
+	
+	public boolean populationDead() {
+	}
+	
+	private ObstacleInfo getClosestObstacle(List<Obstacle> obstacles) {
+	}
 }
+
+
